2844f47a368919f2945f4a2aa4e45d60fed0c5e7
jerlysson.miranda <jerlysson.miranda@newconsoftware.com.br>
2022-09-28 17:33:49 -0300

    feat: Finalizando desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Executando o Nokia");
+Nokia nokia = new Nokia("99999-11111", "1100", "1219219291291921921", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("telegram");
+
+Console.WriteLine("\n");
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine("\n");
+
+Console.WriteLine("Executando o AiFone");
+Iphone iphone = new Iphone("999999-22222", "13", "9291291291299121291", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("whatsapp");
